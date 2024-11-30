185963e49419ac2cb055fdde1f53ddd845af9143
Tian Sanjaya Junaedi <122193371+Jayaaa67@users.noreply.github.com>
2024-11-30 23:34:59 +0700

    Delete config directory

--- config/storage.js
+++
@@ -1,7 +0,0 @@
-const { Storage } = require('@google-cloud/storage');
-require('dotenv').config();
-
-const storage = new Storage({
-  projectId: process.env.capstone-fish-guard,
-  keyFilename: process.env.GOOGLE_CLOUD_KEYFILE
-});
