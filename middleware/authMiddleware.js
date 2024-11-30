4449bf51237af239df5b6d1c6e33b64912ea5851
Tian Sanjaya Junaedi <122193371+Jayaaa67@users.noreply.github.com>
2024-12-01 00:16:40 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -18,8 +18,4 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 module.exports = authMiddleware;
