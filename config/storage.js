f74bb24b56fdae57399b73f0ae3175146c651825
Tian Sanjaya Junaedi <122193371+Jayaaa67@users.noreply.github.com>
2024-11-30 23:29:02 +0700

    Update storage.js

--- config/storage.js
+++ config/storage.js
@@ -2,6 +2,6 @@ const { Storage } = require('@google-cloud/storage');
 require('dotenv').config();
 
 const storage = new Storage({
-  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  projectId: process.env.capstone-fish-guard,
   keyFilename: process.env.GOOGLE_CLOUD_KEYFILE
 });
