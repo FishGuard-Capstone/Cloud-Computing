9175882dcdd6fb84a75c9f2e78f813b44e93b63a
Tian Sanjaya Junaedi <122193371+Jayaaa67@users.noreply.github.com>
2024-12-01 00:16:01 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.34.101.90.19,
+  user: process.env.root,
+  password: process.env.fishguard67,
+  database: process.env.fishguarddb,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
