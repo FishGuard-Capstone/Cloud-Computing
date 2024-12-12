8ed40ae6ed0cac829493d2b52155ddcd61cd53bc
Tian Sanjaya Junaedi <122193371+Jayaaa67@users.noreply.github.com>
2024-12-12 11:24:33 +0700

    Back-End

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/story', storyRoutes);
 // Error handling middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
