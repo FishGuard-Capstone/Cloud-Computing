5e0c86ee2a6c8ab868473b899d95cbc4f4f8272e
Tian Sanjaya Junaedi <122193371+Jayaaa67@users.noreply.github.com>
2024-12-13 16:50:24 +0700

    Back-End Api

--- server.js
+++ server.js
@@ -182,6 +182,7 @@ app.post('/stories', upload, async (req, res) => {
         photo: publicUrl,
         lat: lat ? parseFloat(lat) : null,
         lon: lon ? parseFloat(lon) : null,
+        createdAt: Firestore.Timestamp.now(),
       });
 
       return res.status(201).json({ 
@@ -202,7 +203,7 @@ app.post('/stories', upload, async (req, res) => {
 // Endpoint untuk mendapatkan semua story
 app.get('/stories', async (req, res) => {
   try {
-    const snapshot = await firestore.collection('stories').orderBy('desc').get();
+    const snapshot = await firestore.collection('createdAt', 'stories').orderBy('desc').get();
     if (snapshot.empty) {
       return res.status(404).json({ error: 'No stories found' });
     }
